94a0e13bb01ee73995839889e78f11c72eb862cc
Miles Erickson <miles.erickson@gmail.com>
2019-04-16 13:38:05 -0700

    Add snakes

---
+++ snakes.py
@@ -0,0 +1,24 @@
+class Snake:
+    """A dangerous and/or harmless serpent."""
+    pass
+
+
+class Cobra(Snake):
+    """Definitely dangerous, yup."""
+    
+    def bite(self, other):
+        """Deliver a dose of venom."""
+        pass
+
+    
+class BoaConstrictor(Snake):
+    """This one gives really good hugs."""
+    
+    def squeeze(self, other):
+        """Give a hug."""
+        pass
+
+    
+class BoatConstrictor(BoaConstrictor):
+    """Loose snakes sink ships?"""
+    pass
